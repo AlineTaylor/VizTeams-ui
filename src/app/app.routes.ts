981f4da9b4db3cc9464dfc3838664451cb813414
Aline Yui Taylor <alineyui.taylor@gmail.com>
2025-10-02 19:55:53 -0500

    deployment troubleshooting: enabled standalone component support in app component and updated routing to redirect to login (for now)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { noAuthGuard } from './core/guards/no-auth.guard';
 
 export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
   {
     path: 'login',
     loadComponent: () => import("./features/auth/login/login.component").then((c) => c.LoginComponent),
