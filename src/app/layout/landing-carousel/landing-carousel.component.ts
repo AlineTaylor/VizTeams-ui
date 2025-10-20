203129c875cdfa814cdce1e93e33207677778176
Matthew Higgins <mthiggins1s@semo.edu>
2025-10-19 23:28:09 -0500

    created the carousel

--- src/app/layout/landing-carousel/landing-carousel.component.ts
+++ src/app/layout/landing-carousel/landing-carousel.component.ts
@@ -8,19 +8,4 @@ import { CommonModule } from '@angular/common';
   templateUrl: './landing-carousel.component.html',
   styleUrls: ['./landing-carousel.component.css'],
 })
-export class LandingCarouselComponent {
-  images = [
-  '/carousel_image_1_1080p.png',
-  '/carousel_image_2_1080p.png',
-  '/carousel_image_3_1080p.png',
-  '/carousel_image_4_1080p.png'
-];
-
-  currentIndex = 0;
-
-  constructor() {
-    setInterval(() => {
-      this.currentIndex = (this.currentIndex + 1) % this.images.length;
-    }, 3000);
-  }
-}
+export class LandingCarouselComponent {}
